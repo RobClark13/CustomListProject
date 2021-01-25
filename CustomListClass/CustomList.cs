c2e1150d1634142f176926d611f100ea6d642195
Rob Clark <clarkrob13@gmail.com>
2021-01-25 16:20:56 -0500

    Add Method works and passes all 5 Test methods

--- CustomListClass/CustomList.cs
+++ CustomListClass/CustomList.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace CustomListClass
 {
-    public class CustomList
+    public class CustomList<T>
     {
+        //Member Variables
+        private T[] itemArr = new T[4];
+        public T this[int index]
+        {
+            get => itemArr[index];
+            set => itemArr[index] = value;
+        }
+        private int count;
+        public int Count
+        {
+            get { return count; }
+        }
+        private int capacity=4;
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        //Constructor
+
+
+        //Member Methods
+        public void Add(T item)
+        {
+            if (count == capacity)
+            {
+                T[] increasedArray = new T[capacity *= 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    increasedArray[i] = itemArr[i];
+                }
+                itemArr = increasedArray;
+            }
+            itemArr[count] = item;
+            count++;
+        }
+
+        public void Remove()
+        {
+
+        }
     }
 }
--- CustomListUnitTests/AddMethodTests.cs
+++ CustomListUnitTests/AddMethodTests.cs
@@ -18,13 +18,94 @@ namespace CustomListUnitTests
         public void AddItemToCustomList_CheckCountEquals1()
         {
             //Arrange
-            CustomList list = new CustomList();
+            CustomList<string> list = new CustomList<string>();
             string shoe = "Jordans";
+            int actual;
+            int expected = 1;
             //act
+            list.Add(shoe);
+            actual = list.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddItemToCustomList_CheckForItemAtIndex0()
+        {
+            //arrange
+            CustomList<string> list = new CustomList<string>();
+            string expected = "Pippin";
+            string actual;
+            //act
+            list.Add(expected);
+            actual = list[0];
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void AddItemstoCustomList_CheckForItemsAtIndex3()
+        {
+            //Arrange
+            CustomList<string> list = new CustomList<string>();
+            string name1 = "Stephon";
+            string name2 = "Marbella";
+            string name3 = "JT";
+            string name4 = "Rob";
+            string expected = "Rob";
+            string actual;
+            //ACt
+            list.Add(name1);
+            list.Add(name2);
+            list.Add(name3);
+            list.Add(name4);
+
+            actual = list[3];
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddItemsToCustomList_CheckCapacity()
+        {
+            //arrange
+            CustomList<string> list = new CustomList<string>();
+            string seamus = "Seamus";
+            string daisy = "Daisy";
+            string cody = "Cody";
+            string nash = "Nash";
+            string beethoven = "Beethoven";
+            string bandit = "Bandit";
+            int actual;
+            int expected = 8;
+
+            //act
+            list.Add(seamus);
+            list.Add(daisy);
+            list.Add(cody);
+            list.Add(nash);
+            list.Add(beethoven);
+            list.Add(bandit);
+            actual = list.Capacity;
 
             //assert
+            Assert.AreEqual(expected, actual);
+        }
 
-
+    [TestMethod]
+    public void AddIntegerToCustomList_CheckCountEquals1()
+        {
+            //arrange
+            CustomList <int> list = new CustomList<int>();
+            int thirteen = 13;
+            int expected = 1;
+            int actual;
+            //act
+            list.Add(thirteen);
+            actual = list.Count;
+            //assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
