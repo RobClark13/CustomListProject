298b7ab4536a819c81bee1735b41cb04a76abb6d
Rob Clark <clarkrob13@gmail.com>
2021-01-25 11:35:42 -0500

    Intial commit after adding 3 test methods for the Add method

---
+++ CustomListUnitTests/AddMethodTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListClass;
+
+namespace CustomListUnitTests
+{
+    [TestClass]
+    public class AddMethodTests
+    {
+        
+        //How do we know we've successfully added somthing to our CustomList
+        //Count goes up whwen we add?
+        //test taht the item was added to the right index (indexes outside of zero)
+        //test for different types of data working correctly
+        //
+        //
+        [TestMethod]
+        public void AddItemToCustomList_CheckCountEquals1()
+        {
+            //Arrange
+            CustomList list = new CustomList();
+            string shoe = "Jordans";
+            //act
+
+            //assert
+
+
+        }
+    }
+}
---
+++ CustomListUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CustomListUnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68eb46d1-8bc0-4994-8e53-8a285e3e4ace")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
